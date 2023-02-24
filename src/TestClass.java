04ce35c3a6587252443ae0c20ba11904cac66337
yunwoo0301 <browniris7@naver.com>
2023-02-25 00:53:57 +0900

    홈 작업 테스트

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class TestClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("별의 갯수를 입력하세요 : ");
-        int number = sc.nextInt();
-        for (int i = 0; i < number; i++) {
-            System.out.printf("|%d| ", i);
-            for (int j = 0; j < number; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
+        System.out.print("년도를 입력하세요 : ");
+        int year = sc.nextInt();
+        if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + "은 윤년 입니다.");
+        }else {
+            System.out.println(year + "은 윤년이 아닙니다.");
         }
     }
 }
