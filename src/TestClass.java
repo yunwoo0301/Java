d02a6ee6c92c7936d09b1572c4747cf31fd714eb
yunwoo0301 <browniris7@naver.com>
2023-02-25 01:15:56 +0900

    홈 작업 테스트

--- src/TestClass.java
+++ src/TestClass.java
@@ -5,9 +5,9 @@ public class TestClass {
         Scanner sc = new Scanner(System.in);
         System.out.print("년도를 입력하세요 : ");
         int year = sc.nextInt();
-        if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + "은 윤년 입니다.");
-        }else {
+        } else {
             System.out.println(year + "은 윤년이 아닙니다.");
         }
     }
