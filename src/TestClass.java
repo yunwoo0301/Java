e7dd3daf766416f3cda0f51202af64bd34269472
yunwoo0301 <browniris7@naver.com>
2023-02-25 00:50:48 +0900

    홈 작업 테스트

--- src/TestClass.java
+++ src/TestClass.java
@@ -5,9 +5,9 @@ public class TestClass {
         Scanner sc = new Scanner(System.in);
         System.out.print("별의 갯수를 입력하세요 : ");
         int number = sc.nextInt();
-        for(int i = 0; i < number; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.printf("|%d| ", i);
-            for(int j = 0; j < number; j++) {
+            for (int j = 0; j < number; j++) {
                 System.out.print("* ");
             }
             System.out.println();
